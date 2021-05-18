022fcf95805479d0368c5e45e9d2ed16e652233c
Lari Souza <larissaccsouza@gmail.com>
2021-05-18 20:47:55 -0300

    atualiza atividade6

--- atividade6.js
+++ atividade6.js
@@ -1,27 +1,29 @@
 // 6 - Elabore um algoritmo que receba um número (1-7) e devolva o dia da semana correspondente.
 
-let entrada = 12;
+let entrada = 3;
 
 if (entrada === 1) {
   console.log("Bom domingo!");
 }
-if (entrada === 2) {
+else if (entrada === 2) {
   console.log("Boa segunda-feira!");
 }
-if (entrada === 3) {
+else if (entrada === 3) {
   console.log("Boa terça-feira!");
 }
-if (entrada === 4) {
+else if (entrada === 4) {
   console.log("Boa quarta-feira!");
 }
-if (entrada === 5) {
+else if (entrada === 5) {
   console.log("Boa quinta-feira!");
 }
-if (entrada === 6) {
+else if (entrada === 6) {
   console.log("Boa sexta-feira!");
 }
-if (entrada === 7) {
+else if (entrada === 7) {
   console.log("Bom sábado!");
+} else {
+  console.log ("ERROR 404")
 }
 
 //opção 2
